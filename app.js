abebb1e8d28f8383029770a0a8d1dbce0eef2ead
Oliver <77171670+Fearilion@users.noreply.github.com>
2023-09-09 20:30:39 +0200

    Update 1.0.1

---
+++ app.js
@@ -0,0 +1,18 @@
+const testId = document.getElementById('testId');
+const buttonOne = document.getElementById('buttonOne');
+
+const buttonTwo = document.getElementById('buttonTwo');
+const dropdown = document.getElementById('dropdown');
+
+let state = false;
+buttonOne.onclick = function () {
+    state = !state;
+    testId.innerHTML = state
+        ? 'first test website, to learn everything I need to know for HTML, CSS and JS. Please follow a long while I learn.'
+        : '';
+};
+
+buttonTwo.onclick = function () {
+    dropdown.style.display =
+        dropdown.style.display === 'none' ? 'block' : 'none';
+};
