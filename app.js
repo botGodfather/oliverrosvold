0d10163718bb8b0e0ed1eb1d89e4cb61015aac54
Oliver <77171670+Fearilion@users.noreply.github.com>
2023-09-10 12:20:24 +0200

    Update 1.0.2

--- app.js
+++ app.js
@@ -8,11 +8,13 @@ let state = false;
 buttonOne.onclick = function () {
     state = !state;
     testId.innerHTML = state
-        ? 'first test website, to learn everything I need to know for HTML, CSS and JS. Please follow a long while I learn.'
+        ? 'first test website, to learn most of the stuff I need to know for HTML, CSS and JS. Please follow a long while I learn.'
         : '';
 };
 
+/*
 buttonTwo.onclick = function () {
     dropdown.style.display =
         dropdown.style.display === 'none' ? 'block' : 'none';
 };
+*/
